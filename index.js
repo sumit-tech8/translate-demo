5a25801372e95aae00f494e4e87937297c8eaf98
sumit-tech8 <134231779+sumit-tech8@users.noreply.github.com>
2024-02-24 18:52:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { translate } = require("@vitalets/google-translate-api");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware to parse JSON request body
 app.use(express.json());
